f617006d7f94f1a894ffc54407e205d4f7220918
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-07-31 16:30:52 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# DragonBoost
+
+increase Qualcomm Snapdragon powered devices up to 75% and reduces power consumption up to 25%.
+
+⛔️ Snapdragon only!
+⛔️ Arm & Arm64
+
+## Installation
+
+- Flash in magisk, require magisk v23.0 or higher
+
+## How it works
+
+Changes values found in the stock kernel, custom kernel, and build.prop, increasing gpu speed and speeding up device performance!
---
+++ install.sh
@@ -0,0 +1,32 @@
+SKIPMOUNT=false
+PROPFILE=true
+POSTFSDATA=true
+LATESTARTSERVICE=true
+
+REPLACE_EXAMPLE="
+/system/app/Youtube
+/system/priv-app/SystemUI
+/system/priv-app/Settings
+/system/framework
+"
+
+REPLACE="
+"
+
+print_modname() {
+  ui_print "*************************************"
+  ui_print " DragonBoost v1.0"
+  ui_print ""
+  ui_print " By @Zyarexx | Telegram "
+  ui_print " ⚠️ Snapdragon Only "
+  ui_print "*************************************"
+}
+
+on_install() {
+  ui_print "- DragonBoost Activated!"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+}
