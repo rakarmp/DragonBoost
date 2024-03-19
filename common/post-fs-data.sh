2cb527a5228a9ee967bdb58a66f2409dfbff7c73
ZyarexxByte (良香) <raka_088@smkassalaambandung.sch.id>
2024-03-19 11:06:22 +0700

    Update post-fs-data.sh, Hyper, multithread delete

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -20,8 +20,8 @@ write /proc/sys/vm/page-cluster 0
 write /sys/block/zram0/max_comp_streams 4
 
 # Settingan Untuk Mempercepat GPU Ke performance
-setprop debug.hwui.renderer skiagl
 setprop debug.composition.type c2d
+setprop debug.composition.type gpu
 setprop debug.enabletr true
 setprop debug.overlayui.enable 1
 setprop debug.performance.tuning 1
@@ -29,6 +29,7 @@ setprop hw3d.force 1
 setprop hwui.disable_vsync true
 setprop hwui.render_dirty_regions false
 setprop persist.sys.composition.type c2d
+setprop persist.sys.composition.type gpu
 setprop persist.sys.ui.hw 1
 setprop ro.config.enable.hw_accel true
 setprop ro.product.gpu.driver 1
@@ -44,6 +45,3 @@ setprop ro.vendor.qti.sys.fw.bg_apps_limit 120
 setprop ro.vendor.qti.sys.fw.bservice_enable true
 setprop ro.vendor.qti.core.ctl_max_cpu 4
 setprop ro.vendor.qti.core.ctl_min_cpu 2
-
-setprop persist.sys.dalvik.hyperthreading true
-setprop persist.sys.dalvik.multithread true
